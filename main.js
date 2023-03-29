11eeb5adc923322b2a19c1b5bb16a6f7c89d954f
Zé Aroldo <73472945+zeharoldoparente@users.noreply.github.com>
2023-03-29 13:56:38 -0400

    finalização das funcionalidades do main.js

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const activity = [];
 const notes = [];
 const spanAproved = '<span class="resultado aprovado"> Aprovado </span>'
 const spanReproved = '<span class="resultado reprovado"> Reprovado </span>'
+const minimumNote = parseInt(prompt('Digite a nota mínima para o cálculo'));
 
 let lines = '';
 
@@ -23,16 +24,21 @@ function createLine() {
     const inputNameActivity = document.getElementById('nome-atividade');
     const inputNoteActivity = document.getElementById('nota-atividade');
 
+    if (activity.includes(inputNameActivity.value)) {
+      alert('Esta atividade já foi inserida!');
+  }else{
     activity.push(inputNameActivity.value);
     notes.push(parseFloat(inputNoteActivity.value));
-
+  
     let line = '<tr>';
     line += `<td>${inputNameActivity.value}</td>`;
     line += `<td>${inputNoteActivity.value}</td>`;
-    line += `<td>${inputNoteActivity.value >= 7 ? imgAprovado : imgReprovado}</td>`;
+    line += `<td>${inputNoteActivity.value >= minimumNote ? imgAprovado : imgReprovado}</td>`;
     line += ``;
-
+  
     lines += line;
+  };
+
     
     inputNameActivity.value = '';
     inputNoteActivity.value = '';
@@ -48,17 +54,17 @@ function updateFinalAvarage() {
     const avarageFinal = calculateAvarageFinal();
 
     document.getElementById('avarage-final-value').innerHTML = avarageFinal;
-    document.getElementById('avarage-final-result').innerHTML = avarageFinal >= 7 ? spanAproved : spanReproved;
-    
-}
+    document.getElementById('avarage-final-result').innerHTML = avarageFinal >= minimumNote ? spanAproved : spanReproved;   
+};
 
 function calculateAvarageFinal() {
     let sumNotes = 0;
 
     for (let index = 0; index < notes.length; index++) {
-        sumNotes += notes[index];
-        
+        sumNotes += notes[index];  
     }
 
-    return sumNotes/notes.length;
-}
+    const avarage = sumNotes/notes.length;
+    const avarageFixed = avarage.toFixed(2);
+    return parseFloat(avarageFixed);
+};
