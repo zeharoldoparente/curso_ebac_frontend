32c28e011f4b22c50e088f60a8176beda0f18487
Zé Aroldo <joseharoldoparente@gmail.com>
2024-02-20 16:36:02 -0400

    criação de função em ts

---
+++ src/funcao.ts
@@ -0,0 +1,12 @@
+// Função de multiplicação
+function multiplicacao(x: number, y: number): number {
+   return x * y;
+}
+
+// Função de saudação
+function saudacao(nome: string): string {
+   return "Olá " + nome;
+}
+
+console.log(multiplicacao(5, 3));
+console.log(saudacao("João"));
